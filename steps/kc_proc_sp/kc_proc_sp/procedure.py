a9e2c2e1b9a96a509d1ea60c4f95023121ea239b
kjcohen <38214257+kjcohen@users.noreply.github.com>
2025-03-17 14:25:59 -0400

    Update procedure.py

--- steps/kc_proc_sp/kc_proc_sp/procedure.py
+++ steps/kc_proc_sp/kc_proc_sp/procedure.py
@@ -8,28 +8,21 @@
 # SNOWFLAKE ADVANTAGE: Python Stored Procedures
 
 import time
+import sys
 from snowflake.snowpark import Session
 #import snowflake.snowpark.types as T
 import snowflake.snowpark.functions as F
 
 
 
-def main(session: Session) -> str:
-    return f"Successfully processed ORDERS!"
-    # Create the ORDERS table and ORDERS_STREAM stream if they don't exist
-#     if not table_exists(session, schema='HARMONIZED', name='ORDERS'):
-#         create_orders_table(session)
-#         create_orders_stream(session)
-
-#     # Process data incrementally
-#     merge_order_updates(session)
-# #    session.table('HARMONIZED.ORDERS').limit(5).show()
-
-#     return f"Successfully processed ORDERS!"
+def main(inputstring: str) -> str:
+    return str(inputstring)
 
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
-    # Create a local Snowpark session
-    return f"Successfully processed ORDERS!"
+    if len(sys.argv) > 1:
+        print(main(*sys.argv[1:]))  # type: ignore
+    else:
+        print(main())  # type: ignore
